c18cebdfc0f7ed97f1a2c05b6aaf7dd952bf7f1e
byeongjuPark <bottlepark28@gmail.com>
2022-11-16 15:17:30 +0900

    1

---
+++ docs/vanilla_js/arrays.js
@@ -0,0 +1,20 @@
+// Array == ArrayList
+
+let fruits = ["apple", "banana", "orange", "melon"];
+let numbers = [1, 2, 3, 4, 5];
+let fruits_init = new Array();
+fruit_init = [];
+
+//object == HashMap
+let object = { name: "Tom", age: 4 };
+
+let mixins_first = [1, 2, 3, "apple", 4, "melon"];
+let mixins_second = [1, 2, 3, "apple", { name: "Tom", age: 4 }];
+let mixins_third = [1, 2, 3, "banana", object];
+const mixins_array = [
+  ["apple", "banana"],
+  ["orange", "mellon"],
+  ["berry", "strawberry"],
+];
+
+fconsole.log();
---
+++ docs/vanilla_js/functions_sums.js
@@ -0,0 +1,6 @@
+function sum(parameter) {
+  let sum = 0;
+  for (let i of parameter) {
+    sum += parameter[i];
+  }
+}
