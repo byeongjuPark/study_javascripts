a7ac957fc0b02fb297162fa957ad999644ac8163
byeongjuPark <bottlepark28@gmail.com>
2022-11-16 15:23:17 +0900

    1

--- docs/vanilla_js/functions_sums.js
+++ docs/vanilla_js/functions_sums.js
@@ -1,6 +1,11 @@
-function sum(parameter) {
+function sumF(parameter) {
   let sum = 0;
   for (let i of parameter) {
-    sum += parameter[i];
+    sum += i;
   }
+  return sum;
 }
+
+let number = [1, 2, 3, 4, 5, 6, 7];
+let result = sumF(number);
+console.log(result);
