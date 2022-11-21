fb2618641c66dfdf61985f013ecbde6d21a78d0f
byeongjuPark <bottlepark28@gmail.com>
2022-11-21 12:29:38 +0900

    11

--- docs/vanilla_js/test.js
+++ docs/vanilla_js/test.js
@@ -1,5 +1,5 @@
-const fs = require("fs");
+let fs = require("fs");
 
 const filepath =
   process.platform === "linux" ? "/dev/stdin" : "docs/vanilla_js/input.txt";
-let inputs = fs.readFileSync(filepath).toString().trim().split(" ").map(Number);
+let input = fs.readFileSync(filepath).toString().trim().split("\n").map(Number);
